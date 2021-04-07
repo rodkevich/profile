f678c2fc04c40c8395785b509a1bb036b5154c76
404 <ivan@test.it>
2021-04-08 01:28:34 +0300

    fix: naming and styling

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"os"
+
+	"github.com/rodkevich/profile/repository"
+	"github.com/rodkevich/profile/repository/postgres"
 )
 
 func main() {
 	_ = os.Setenv("DATABASE_URL", "postgresql://postgres:postgres@0.0.0.0:5432/postgres")
+
+	var (
+		rep repository.Repository
+		err error
+	)
+
+	rep, err = postgres.NewRepository() //init postgres rep db
+
+	if err != nil {
+		panic(err)
+	}
+
+	//create tables if not presented
+	err = rep.Up()
+	if err != nil {
+		panic(err)
+	}
 }
